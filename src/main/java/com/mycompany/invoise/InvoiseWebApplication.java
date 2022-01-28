7a565c18e2da49649e3ac441456ed386db199580
Dkdat1208 <deograciadilou@yahoo.fr>
2022-01-28 15:40:10 +0100

    added mysql-connector and spring jdbc dependancies, connection with mysql database

--- src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
+++ src/main/java/com/mycompany/invoise/InvoiseWebApplication.java
@@ -2,12 +2,42 @@ package com.mycompany.invoise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class InvoiseWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InvoiseWebApplication.class, args);
+    	ApplicationContext  context =  SpringApplication.run(InvoiseWebApplication.class, args);
+		DataSource ds = context.getBean(DataSource.class);
+		Connection connection = null ;
+		try {
+			connection =  ds.getConnection();
+			System.out.println("succes");
+			ResultSet rs  = connection.createStatement().executeQuery("SELECT INVOICE_NUMBER, CUSTOMER_NAME  FROM   INVOICE");
+			while(rs.next()){
+				System.out.println(rs.getLong("INVOICE_NUMBER")+" | "+ rs.getString("CUSTOMER_NAME"));
+			}
+
+		} catch (SQLException e){
+			e.printStackTrace();;
+		}
+		finally {
+			try{
+				if(connection != null) {
+					connection.close();
+				}
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+
+
 	}
 
 }
